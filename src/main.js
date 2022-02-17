04fe5f51857211d89de64e82f090d8e02fbec5cc
Matthew Weir <matthew.weir@mcvts.org>
2022-02-17 09:05:32 -0500

    it's done???

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+/**
+ * The Shoot-Down! Created as an AP Performance Task on 2/17/22.
+ * @author Matthew Weir
+ * @version 1.0.0
+ */
+
 import kaboom from "kaboom";
 
 // Initialize the game
@@ -8,14 +14,19 @@ onLoad(() => canvas.focus());
 
 // Determines if enemies should be spawning
 let enemySpawn = false;
+// The offset of the game's time from the start of each round's time
 let timeOffset = 0;
+// Used to cancel the draw and update functions in the main game
 let cancelOnDraw, cancelOnUpdate;
-let scoreHistory = getData("scoreHistory") || [];
+// The previous history of scores
+let scoreHistory = getData("scoreHistory", []);
 
+// Calculates the time a round has been running
 function activeTime() {
     return time() - timeOffset;
 }
 
+// Helper function to add a text object.
 function addText(content, size, x, y, props = []) {
     return add([
         text(content, {size}),
@@ -25,19 +36,25 @@ function addText(content, size, x, y, props = []) {
     ].concat(props));
 }
 
-// Add the hint text in the beginning of the game
-const hint1 = addText("WASD to move", 50, center().x, center().y - 150, [origin("center"), stay()]);
-const hint2 = addText("click to shoot", 50, center().x, center().y - 100, [origin("center"), stay()]);
-
 scene("game", () => {
-    let started = false;
-    hint1.text = "WASD to move";
-    hint2.text = "click to shoot"
+    // Hint text that shows you how to play
+    const hint1 = addText("WASD to move", 50, center().x, center().y - 150, [origin("center")]);
+    const hint2 = addText("click to shoot", 50, center().x, center().y - 100, [origin("center")]);
 
+    // Add the time alive, and hide it until the round starts
+    const score = addText("time alive: 0", 30, 15, 15);
+    score.hidden = true;
+
+    // Whether the round has started
+    let started = false;
+
+    // Starts the round
     function start() {
+        // Start spawning enemies and update some variables
         enemySpawn = true;
         started = true;
         timeOffset = time();
+        // Hide the hint text and show the time alive
         score.hidden = false;
         hint1.hidden = true;
         hint2.hidden = true;
@@ -45,12 +62,14 @@ scene("game", () => {
 
     // Draw the health bar
     function drawHealthBar(obj) {
+        // Draw the white full length bar
         drawLine({
             p1: obj.pos.sub(obj.radius, obj.radius * 1.75),
             p2: obj.pos.sub(-obj.radius, obj.radius * 1.75),
             width: 5,
             color: WHITE
         });
+        // Draw the green bar that represents how much health the player has
         drawLine({
             p1: obj.pos.sub(obj.radius, obj.radius * 1.75),
             p2: obj.pos.sub(-(obj.radius * 2 * obj.hp() / 10 - obj.radius), obj.radius * 1.75),
@@ -59,11 +78,7 @@ scene("game", () => {
         });
     }
 
-    // Add the time alive
-    const score = addText("time alive: 0", 30, 15, 15);
-
-    score.hidden = true;
-
+    // Every frame, update the time alive display with the time alive
     score.onUpdate(() => {
         score.text = `time alive: ${Math.floor(activeTime())}`
     });
@@ -84,7 +99,9 @@ scene("game", () => {
         solid(),
         "player"
     ]);
+    player.indicatorPos = vec2();
 
+    // When the player dies, go to the game over screen
     player.onDeath(() => go("gameOver"));
 
     // Add the outer walls so solids (such as the player and enemies) can't leave the view area
@@ -121,8 +138,6 @@ scene("game", () => {
         solid()
     ]);
 
-    player.indicatorPos = vec2();
-
     // Handle movement
     onKeyDown("w", () => {
         player.moveBy(0, -6);
@@ -147,6 +162,7 @@ scene("game", () => {
     // Handle shooting
     onMousePress(mousePos => {
         const angle = mousePos.angle(player.pos);
+        // Create a bullet at the indicator position, moving indefinitely towards the mouse, which gets destroyed when it leaves view
         add([
             circle(5),
             area({
@@ -176,16 +192,21 @@ scene("game", () => {
             angle,
             fill: true
         });
+        // Draw the player's health bar
         drawHealthBar(player);
+        // Draw each enemy's health bar
         every("enemy", drawHealthBar);
     });
 
     cancelOnUpdate = onUpdate(() => {
+        // Every frame, if a random number from 0 to 1 is greater than 0.99 (and enemy spawning is enabled) spawn an enemy.
         if (rand() > 0.99 && enemySpawn) {
             let enemyPos;
             do {
+                // Calculate a random enemy position, and make sure that the enemy doesn't spawn on top of the player
                 enemyPos = vec2(randi(width()), randi(height()))
             } while (player.hasPoint(enemyPos));
+            // Spawn the enemy
             const enemy = add([
                 circle(10),
                 pos(enemyPos),
@@ -200,36 +221,73 @@ scene("game", () => {
                 health(10),
                 "enemy"
             ]);
+            // Every frame, move the enemy towards the player at a speed depending on how long the round has been active for.
+            // Also heal the enemy
             enemy.onUpdate(() => {
                 enemy.moveTo(player.pos, 30 + activeTime());
                 if (enemy.hp() < 10) enemy.heal(0.01);
             });
+            // If the enemy hits the player, destory the enemy and damage the player.
             enemy.onCollide("player", () => {
                 enemy.destroy();
                 player.hurt();
             });
+            // If a bullet hts the player, destroy the bullet and hurt the enemy.
             enemy.onCollide("bullet", (bullet) => {
                 enemy.hurt(5);
                 bullet.destroy();
             });
+            // If the enemy dies, destroy it.
             enemy.onDeath(enemy.destroy);
         }
     });
 });
 
 scene("gameOver", () => {
+    // Get the round's active time, push it to the history, and save the history.
     let timeAlive = Math.floor(activeTime());
     scoreHistory.push(timeAlive);
-    hint1.text = "game over"
-    hint2.text = "time alive: " + timeAlive
-    hint1.hidden = false;
-    hint2.hidden = false
-    addText("press space to restart", 50, center().x, center().y, [origin("center")]);
-    enemySpawn = false;
+    setData("scoreHistory", scoreHistory);
+    // If the active time is greater than the previous high score, save the new time as the high score.
+    if (timeAlive > getData("highScore", timeAlive)) setData("highScore", timeAlive);
+    // Stop drawing the health bars and player indicator, and stop checking for game updates.
     cancelOnDraw();
     cancelOnUpdate();
-
-    onKeyPress("space", () => go("game"));
+    // Stop spawning enemies
+    enemySpawn = false;
+    // Go to the game over screen itself
+    go("gameOverScreen");
 });
 
+scene("gameOverScreen", () => {
+    // Add the game over screen's text
+    addText("game over", 50, center().x, center().y - 150, [origin("center")]);
+    addText("time alive: " + scoreHistory[scoreHistory.length - 1], 50, center().x, center().y - 100, [origin("center")]);
+    addText("press space to restart", 50, center().x, center().y, [origin("center")]);
+    addText("press S to view past scores", 50, center().x, center().y + 50, [origin("center")]);
+
+    // If space is pressed, start a new round
+    onKeyPress("space", () => go("game"));
+    // If s is pressed, view the scores
+    onKeyPress("s", () => go("scores"));
+})
+
+scene("scores", () => {
+    // Show last round's score and the current high score
+    addText("scores", 100, center().x, 75, [origin("center")]);
+    addText("score: " + scoreHistory[scoreHistory.length - 1], 50, center().x, 150, [origin("center")]);
+    addText("high: " + getData("highScore", scoreHistory[scoreHistory.length - 1]), 50, center().x, 200, [origin("center")]);
+
+    // Display all the past scores, with up to 5 scores per line
+    addText("past scores", 75, center().x, 300, [origin("center")]);
+    for (let i = 0; i < Math.ceil(scoreHistory.length / 5); i++) {
+        addText(scoreHistory.slice(0, -1).slice(i * 5, i * 5 + 5).join(", ") + ((i + 1) * 5 < scoreHistory.length - 1 ? "," : ""), 50, center().x, 375 + 50 * i, [origin("center")]);
+    }
+
+    // If space is pressed, go back to the game over screen
+    addText("press space to return", 50, center().x, height() - 100, [origin("center")]);
+    onKeyPress("space", () => go("gameOverScreen"));
+});
+
+// Go to the game screen to start
 go("game");
